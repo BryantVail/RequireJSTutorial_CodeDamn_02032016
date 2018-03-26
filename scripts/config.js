2a75d1de849855b2f7a7787cffb9927aa511a450
bxvail <BryantVail@gmail.com>
2018-03-26 02:23:37 -0400

    requirejs.config({})

---
+++ scripts/config.js
@@ -0,0 +1,67 @@
+//config.js
+"strict mode";
+
+requirejs.config({
+    baseUrl:"lib",
+    paths:{
+        react:"react",
+        jquery:"jquery"
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
